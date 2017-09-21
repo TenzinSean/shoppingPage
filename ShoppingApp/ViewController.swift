5b7057f8c94a8428d90a5fc201f2eb8d59d6e226
tenzinsean <tenzin.jeunesse@gmail.com>
2017-09-21 12:23:08 +0200

    first commit

---
+++ ShoppingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoppingApp
+//
+//  Created by Tenzin Sean on 9/21/17.
+//  Copyright © 2017 Tenzin Sean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
